c56f951251581347d13c6ffa3415f39b97c863d8
Ricardo Vásquez <rvasquezpl@gmail.com>
2022-06-21 05:25:59 -0500

    Ejericio N 2

---
+++ unidad2/ejercicio2.js
@@ -0,0 +1,19 @@
+let nombreCompleto = "RICARDO ALBERTO VASQUEZ PLASENCIA";
+let edad = 31;
+let programador = true;
+let FNacimiento = new Date(1990,06,22);
+const libroFavorito ={
+    titulo : "Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming",
+    autor : "Marijn Haverbeke",
+    año : "2018",
+    url : "https://www.amazon.com/-/es/Marijn-Haverbeke/dp/1593279507/ref=sr_1_3?keywords=javascript+the+good+parts&qid=1655806909&s=books&sprefix=javascript+the+g%2Cstripbooks-intl-ship%2C220&sr=1-3"
+}
+console.log(FNacimiento)
+
+const DatosPersonales=[
+    nombreCompleto,
+    edad,
+    programador,
+    FNacimiento,
+    libroFavorito
+];
